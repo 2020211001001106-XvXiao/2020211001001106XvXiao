d125e04a701d4f141142952605f2f5396ca6d834
2020211001001106-XvXiao <319677245@qq.com>
2022-04-23 14:26:55 +0800

    week9 works

--- src/main/java/com/XvXiao/week2/HelloWorldServlet.java
+++ src/main/java/com/XvXiao/week2/HelloWorldServlet.java
@@ -1,9 +1,11 @@
 package com.XvXiao.week2;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 
 public class HelloWorldServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
